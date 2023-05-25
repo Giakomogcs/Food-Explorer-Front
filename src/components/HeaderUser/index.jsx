b39150f9de26ae2fd06cd638c2c613710387abf5
Giovani <giakomogcs@gmail.com>
2023-05-24 22:33:29 -0300

    responsive header user

--- src/components/HeaderUser/index.jsx
+++ src/components/HeaderUser/index.jsx
@@ -1,6 +1,9 @@
 import { Container, Menu, Logo, Receipt, Content  } from "./styles";
 
-import {AiOutlineMenu} from'react-icons/ai'
+import {Input} from "../../components/SearchBar"
+
+import {RxExit} from'react-icons/rx'
+import {FiSearch} from 'react-icons/fi'
 
 export function HeaderUser(){
   return(
@@ -9,25 +12,35 @@ export function HeaderUser(){
 
         <Menu>
           <div className="Menu">
-            <img 
-              src="public\images\Menu.svg" alt="icon Menu" 
-            />
+              <img src="public\images\Menu.svg" alt="icon Menu"/>
           </div>
         </Menu>
 
         <Logo>
-          <img 
-            id="Logo"
-            src="public\images\Logo_user.svg" alt="Logo para usuários" 
-          />
+          <div className="Logo">
+            <img 
+              src="public\images\Logo_user.svg" alt="Logo para usuários" 
+            />
+          </div>
         </Logo>
+
+
+        <div className="Search">
+          <Input
+            placeholder = "Busque por pratos ou ingredientes" 
+            icon={FiSearch}
+          />
+        </div>
 
         <Receipt>
           <div className="Receipt">
             <img src="public\images\Receipt.svg" alt="icon de receitas" />
+            <p>Pedidos (0)</p>
             <span>1</span>
           </div>
         </Receipt>
+
+        <RxExit/>
       </Content>
 
     </Container>
--- src/components/HeaderUser/styles.js
+++ src/components/HeaderUser/styles.js
@@ -16,20 +16,64 @@ export const Container = styled.header`
 
   background-color: ${({theme}) => theme.COLORS.DARK_700};
 
+  @media all and (min-width: 768px) {
+  
+  height: 104px;
+  padding: 24px 123px;
+
+  display: flex;
+  align-items: center;
+}
+
 `;
 
 export const Content = styled.div`
 
   height: 34px;
   width: 100%;
+  margin: 0;
 
   display: flex;
+  gap: 32px;
 
   align-items: center;
   justify-content: space-between;
-  
 
   background-color: ${({theme}) => theme.COLORS.DARK_700};
+
+  >.Search{
+    display: none;
+  }
+
+
+  
+  @media all and (max-width: 768px) {
+    
+    >svg{
+      display: none;
+    }
+  }
+  
+  @media all and (min-width: 768px) {
+    justify-content: center;
+    
+    >svg{
+      min-width: 22px;
+      font-size: 22px;
+    }
+  }
+
+  @media all and (min-width: 1024px) {
+    >.Search{
+      display: flex;
+      width: 100%;
+      
+      div{
+        margin-bottom: 0;
+      }
+    }
+  }
+
 
 `;
 
@@ -47,19 +91,24 @@ export const Menu = styled.button`
   border: none;
   background: none;
 
-  > svg{
-    color: ${({theme}) => theme.COLORS.LIGHT_100};
-    
+  > img{
     width: 24px;
     height: 18px;
   }
+
+  @media all and (min-width: 768px) {
+
+    display: none;
+  }
 `;
+
+
 
 export const Receipt = styled.button`
   border: none;
   background: none;
 
-  height: 34px;
+  height: auto;
 
   >div{
     width: 20px;
@@ -89,25 +138,51 @@ export const Receipt = styled.button`
       left: 15px;
       bottom: 35px;
     }
+
+    >p{
+      display: none;
+    }
   }
   
+  
 
-
-  @media(min-width: 768px) {
+  @media all and (min-width: 768px) {
 
     .Content{
+      height: 104px;
       padding: 24px 123px;
-    }
-    
-    
-    .Menu{
-      img{
-        display: none;
-      }
-    }
-    
 
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+
+    
+  
     .Receipt{
+      width: 216px;
+      height: 48px;
+      border-radius: 5px;
+      background-color: ${({theme}) => theme.COLORS.TOMATO_100};
+
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      margin: 0;
+      gap: 8px;
+
+      p{
+        display: flex;
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 24px;
+        color: ${({theme}) => theme.COLORS.LIGHT_100};
+      }
+
       span{
         display: none;
       }
