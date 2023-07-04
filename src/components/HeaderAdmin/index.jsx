0371f536a5005d18f8ea8b6ebd7d5d1f88a3c959
Giovani <giakomogcs@gmail.com>
2023-07-03 22:20:02 -0300

    img header

--- src/components/HeaderAdmin/index.jsx
+++ src/components/HeaderAdmin/index.jsx
@@ -30,18 +30,18 @@ export function HeaderAdmin(){
         <div className="Menu">
           <Menu to="/menu">
             <img 
-              src="../../../public/images/Menu.svg" alt="icon Menu" 
+              src="public\images\Menu.svg" alt="icon Menu" 
             />
           </Menu>
         </div>
 
         <Logo>
           <img className="Logo"
-            src="../../../public/images/Logo_user.svg" alt="Logo para usuários" 
+            src="public\images\Logo_user.svg" alt="Logo para usuários" 
           />
           
           <img 
-            src="../../../public/images/admin.svg" alt="icon Menu" 
+            src="public\images\admin.svg" alt="icon Menu" 
           />
         </Logo>
 
